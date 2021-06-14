69af1ec7fbc57b110994db5727b6bf0f4fd864cd
paladinescamila <camilapaladines27@gmail.com>
2021-06-13 19:31:53 -0500

    Add normalize.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,37 +14,37 @@ export function Login() {
 
     return (
         <Router>
-        <div class="login-container">
-            <form action="#" class="sign-in-form">
-                <h2 class="title">Inicia Sesión</h2>
-                <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Usuario" />
-                </div>
-                <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Contraseña" />
-                </div>
-                <input type="submit" value="Ingresar" class="btn solid" />
-                <p class="social-text">O ingresa con tus redes sociales</p>
-                <div class="social-media">
-                <a href="#" class="social-icon">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>
-                </div>
-                <p>O prefieres <Link to="/signup">registrarte</Link></p>
-            </form>
-            <Route path="/signup" component={Signup} />
-        </div>
+            <div class="login-container">
+                <form action="#" class="sign-in-form">
+                    <h2 class="title">Inicia Sesión</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Usuario" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Contraseña" />
+                    </div>
+                    <input type="submit" value="Ingresar" class="btn solid" />
+                    <p class="social-text">O ingresa con tus redes sociales</p>
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                    <p>¿Aún no tienes una cuenta? <Link to="/signup">Regístrate</Link></p>
+                </form>
+                <Route path="/signup" component={Signup} />
+            </div>
         </Router>
     )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,7 @@
 import '../css/signup.css';
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Login } from './Login';
 
 export function Signup() {
 
@@ -14,37 +16,38 @@ export function Signup() {
         <>
             <div class="signup-container">
                 <form action="#" class="sign-up-form">
-                <h2 class="title">Regístrate</h2>
-                <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Usuario" />
-                </div>
-                <div class="input-field">
-                <i class="fas fa-envelope"></i>
-                <input type="email" placeholder="Correo electrónico" />
-                </div>
-                <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Contraseña" />
-                </div>
-                <input type="submit" class="btn" value="Registrar" />
-                <p class="social-text">O regístrate con tus redes sociales</p>
-                <div class="social-media">
-                <a href="#" class="social-icon">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-google"></i>
-                </a>
-                <a href="#" class="social-icon">
-                    <i class="fab fa-linkedin-in"></i>
-                </a>
-                </div>
-                <p>O prefieres <a onClick={ingresar}>iniciar sesión</a></p>
-            </form>
+                    <h2 class="title">Regístrate</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Usuario" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-envelope"></i>
+                        <input type="email" placeholder="Correo electrónico" />
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Contraseña" />
+                    </div>
+                    <input type="submit" class="btn" value="Registrar" />
+                    <p class="social-text">O regístrate con tus redes sociales</p>
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                    <p>¿Ya tienes una cuenta? <Link to="/login">Inicia Sesión</Link></p>
+                </form>
+                <Route path="/login" component={Login} />
             </div>
         </>
     )
