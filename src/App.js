f58cc1ed4c9c1d0a17473374f3a0c3070c7abeae
paladinescamila <camilapaladines27@gmail.com>
2021-06-14 11:56:07 -0500

    Solved link problems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import './css/App.css';
+import React, {Fragment, useState, useRef, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Login } from './components/Login.jsx';
 import { Signup } from './components/Signup.jsx';
 
 function App() {
+
+  const loginOption = () => {
+    setDisplay(<Login signupOption={signupOption} />);
+  }
+  
+  const signupOption = () => {
+    setDisplay(<Signup loginOption={loginOption} />);
+  }
+  
+  const [display, setDisplay] = useState(<Login signupOption={signupOption} />);
+  
   return (
-    <Login />
+    <>
+      <div class="container">
+        <div class="login-or-signup">
+          {display}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,48 +3,26 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Signup } from './Signup';
 
-export function Login() {
-    
-    let flag = 0;
+export function Login({signupOption}) {
 
-    const registrarse = () => {
-        flag += 1;
-        return flag;
-    }
+    const changeToSignup = () => {
+        signupOption();
+    };
 
     return (
-        <Router>
-            <div class="login-container">
-                <form action="#" class="sign-in-form">
-                    <h2 class="title">Inicia Sesión</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Usuario" />
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Contraseña" />
-                    </div>
-                    <input type="submit" value="Ingresar" class="btn solid" />
-                    <p class="social-text">O ingresa con tus redes sociales</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </div>
-                    <p>¿Aún no tienes una cuenta? <Link to="/signup">Regístrate</Link></p>
-                </form>
-                <Route path="/signup" component={Signup} />
+        <form action="#" class="sign-in-form">
+            <h2 class="title">Inicia Sesión</h2>
+            <div class="input-field">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Usuario" />
             </div>
-        </Router>
+            <div class="input-field">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Contraseña" />
+            </div>
+            <input type="submit" value="Ingresar" class="btn solid" />
+            <p class="signup-message">¿Aún no tienes una cuenta?</p>
+            <div class="signup-button" onClick={changeToSignup}>Regístrate</div>
+        </form>
     )
 }
