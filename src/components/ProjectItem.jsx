9b40c72f1011a20c724855756d5aff84fb3cec52
paladinescamila <camilapaladines27@gmail.com>
2021-06-15 13:42:52 -0500

    Add admin icon to project card

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/ProjectItem.css';
+import adminIcon from '../img/admin.svg';
 
 export default function ProjectItem({project}) {
 
@@ -8,13 +9,16 @@ export default function ProjectItem({project}) {
             <p className="project-card-name">{project.name}</p>
             <p className="project-card-description">{project.description}</p>
             <div className="progress-bar-and-admin">
-                <div>
+                <div className="progress-bar">
                     <div className="project-progress-bar">
                         <div style={{width: project.progress + "%"}}className="project-progress"></div>
                     </div>
                     <p className="progress-number">{project.progress} %</p>
                 </div>
-                <p>{project.admin}</p>
+                <div className="admin-card">
+                    <img src={adminIcon} alt="Project admin"></img>
+                    <p>{project.admin}</p>
+                </div>
             </div>
         </li>
     )
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,7 +5,7 @@ const projects = [
         description: "Este proyecto va a construir una página web.",
         version: "5",
         progress: 25,
-        admin: "Pepito Pérez"
+        admin: "Lorem ipsum"
     },
     {
         id: "Project-002",
@@ -21,7 +21,7 @@ const projects = [
         description: "Este proyecto va a construir una página web.",
         version: "5",
         progress: 57,
-        admin: "Pepito Pérez"
+        admin: "No"
     },
 ];
 
