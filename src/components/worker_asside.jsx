0546cb4019d0bc4109f4a6bcfdf7da0845eb3881
juanjo44 <38504408+juanjo44@users.noreply.github.com>
2021-06-19 15:50:27 -0500

    Se cambia del login al menu de la cuenta

--- src/components/worker_asside.jsx
+++ src/components/worker_asside.jsx
@@ -5,18 +5,17 @@ import AssideAccount from './AssideAccount';
 import Conector_Projects from './Conector_Projects';
 import Conector_Requirements from './Conector_Requirements';
 import IntroductionSection from './IntroductionSection';
+
 function worker_asside() {
     return (
-        <>
         <Router>
         <AssideAccount/>
         <Switch>
-            <Route path='/' exact component={Conector_Projects}/>
+            <Route exact path='/' exact component={Conector_Projects}/>
             <Route path='/requirements' component={Conector_Requirements}/>
             <Route path='/intro' component={IntroductionSection}/>
         </Switch>
         </Router>
-        </>
     )
 }
 
